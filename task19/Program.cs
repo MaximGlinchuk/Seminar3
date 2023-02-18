315b74a16236e9de48b5229d2546410a55803936
Maxim Glinchuk <glinchuk.maxim@yandex.ru>
2023-02-18 14:50:52 +0400

    complete task19

---
+++ task19/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.WriteLine("Введите ПЯТИЗНАЧНОЕ число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string strnum = Convert.ToString(number);
+if (number >= 10000 && number <= 99999)
+{
+    if (strnum[0] == strnum[^1] && strnum[1] == strnum[^2])
+    {
+    Console.WriteLine("Число является полиндромом");
+    }
+    else
+    Console.WriteLine("Число не является полиндромом");
+}
+else
+Console.WriteLine("Число не пятизначное!");
